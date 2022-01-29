254c4ee663f3d2f895b186902edca24599c76fa2
rodrigo-7-maker <rodrigodevianna@gmail.com>
2022-01-29 12:58:18 -0300

    first commit

---
+++ src/TestaPontoFlutuante.java
@@ -0,0 +1,18 @@
+
+public class TestaPontoFlutuante {
+	public static void main(String[] args) {
+		double salario = 1250.70;
+				System.out.println("Meu salário é " + salario + " Reais.");
+		
+		double divisão = 3.14 /2;
+		System.out.println(divisão);
+		
+		int outraDivisao = 5/2;
+		System.out.println(outraDivisao);
+		
+		double novaTentativa = 5.0/2;
+		System.out.println(novaTentativa);
+		
+	}
+
+}
---
+++ src/TestaVariaveis.java
@@ -0,0 +1,9 @@
+
+public class TestaVariaveis {
+	
+	public static void main(String[] args) {
+		 int idade = 25;
+		 System.out.println("A idade é " + idade);
+	}
+
+}
